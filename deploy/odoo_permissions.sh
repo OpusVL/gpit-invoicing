1a664b27093b154a3dce9ebe0e28f83b3a741d12
Ben Leedham <benjamin.ellis.leedham@gmail.com>
2020-08-05 12:07:06 +0100

    updated permissions

--- deploy/odoo_permissions.sh
+++ deploy/odoo_permissions.sh
@@ -5,5 +5,5 @@
 # Parse the .env variables by finding them in the same path as this script.
 eval $(egrep -v '^#' $(dirname `readlink -f $0`)/.env)
 
-sudo chown -Rv `docker-compose exec odoo sh -c "grep odoo /etc/passwd | cut -f3,4 -d:"| sed "s/\r//"` ${CONTAINER_VOLUME}/odoo ./odoo
+sudo chown -Rv 101:101 ${CONTAINER_VOLUME}/odoo ./odoo
 
